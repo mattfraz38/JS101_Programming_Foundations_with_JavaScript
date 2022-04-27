62a68e0f0bf4b64aa2d25eebe767bd0c6f5848aa
Matt Frazier <frazier3388@yahoo.com>
2022-04-27 21:10:22 +0900

    adding the pseudocode file I forgot to save

---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,22 @@
+// Pseudocode
+// -----------------------------------------------------
+// Write a function that returns the sum of two numbers
+
+// Create a function that takes two arguments
+//  - Set a variable for the result
+//  - Save the value of argument_1 and argument_2 added 
+//    together to the the result variable
+// Return the result
+
+// -----------------------------------------------------
+
+// Formal Pseudocode
+// -----------------------------------------------------
+// START
+
+// SET function(num1, num2)
+//  - SET result = null;
+//  - SET result = num1 + num2
+// PRINT result
+
+// END
