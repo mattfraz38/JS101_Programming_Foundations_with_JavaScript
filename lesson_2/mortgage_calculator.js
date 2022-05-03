9f59157e1299363f429cef0cbdad4bcac90e84c4
Matt Frazier <frazier3388@yahoo.com>
2022-05-04 08:53:54 +0900

    Completed monthly calculations and ask user for another calculation

--- lesson_2/mortgage_calculator.js
+++ lesson_2/mortgage_calculator.js
@@ -26,30 +26,75 @@ const invalidAPR = input => {
     (input === Number.NEGATIVE_INFINITY);
 };
 
-// Get loan amount
-let loanAmount = Number(rlSync.question("Loan amount: "));
+console.log('*'.repeat(50));
+console.log('\tWelcome to Mortgage Calcualator!');
+console.log('*'.repeat(50));
 
-while (invalidLoan(loanAmount)) {
-  console.log('Error! Invalid loan amount!');
-  loanAmount = Number(rlSync.question("Loan amount: "));
-}
+let newCalculation = true;
 
-// Get loan month duration
-let loanMonths = Number(rlSync.question("Loan months: "));
+// Continually do mortgage calculations
+// until newCalculation set to false
+do {
+  // Get loan amount
+  let loanAmount = Number(rlSync.question("Loan amount: "));
 
-while ((invalidLoanDuration(loanMonths))) {
-  console.log('Error! Invalid month duration');
-  loanMonths = Number(rlSync.question("Loan months: "));
-}
+  while (invalidLoan(loanAmount)) {
+    console.log('Error! Invalid loan amount!');
+    loanAmount = Number(rlSync.question("Loan amount: "));
+  }
 
-// Get loan APR
-let apr = Number(rlSync.question("APR: "));
+  // Get loan month duration
+  let loanMonths = Number(rlSync.question("Loan months: "));
 
-while (invalidAPR(apr)) {
-  console.log('Error! Invalid APR!');
-  apr = Number(rlSync.question("APR: "));
-}
+  while ((invalidLoanDuration(loanMonths))) {
+    console.log('Error! Invalid month duration');
+    loanMonths = Number(rlSync.question("Loan months: "));
+  }
 
-let monthlyInterestRate = (apr / 100) / loanMonths;
+  // Get loan interest rate
+  let interestRate = Number(rlSync.question("Interest rate: "));
 
-console.log(`Loan amount = ${loanAmount}.\nLoan months = ${loanMonths}.\nAPR: ${apr}%.\nMonthly interest rate: ${monthlyInterestRate}`);
+  while (invalidAPR(interestRate)) {
+    console.log('Error! Invalid interest rate!!');
+    interestRate = Number(rlSync.question("Interest rate: "));
+  }
+
+  // Calcultate monthly interest rate
+  let monthlyInterestRate = (interestRate / 100) / 12;
+
+  // Calculate monthly payment
+  let monthlyPayment = loanAmount *
+    (monthlyInterestRate / (1 - Math.pow((1 +
+      monthlyInterestRate), (-loanMonths))));
+
+  // Round monthly payment to two decimal places
+  monthlyPayment = monthlyPayment.toFixed(2);
+
+  console.log('-'.repeat(50));
+
+  console.log(`Loan amount: $${loanAmount}\nLoan months: ${loanMonths}\nInterest rate: ${interestRate}%\nMonthly interest rate: ${monthlyInterestRate}%`);
+
+  console.log(`Monthly payment: $${monthlyPayment}`);
+
+  console.log('-'.repeat(50));
+
+  // Ask if want to do another calculation
+  let response = rlSync.question('Again? (Yes/n): ').toLowerCase();
+
+  while (response[0] !== 'y' && response[0] !== 'n') {
+    console.log('Error! Invalid response! (Yes/n)');
+    response = rlSync.question('Again? (Yes/n): ').toLowerCase();
+  }
+
+  response = response[0];
+
+  // Change newCalculation value depending on response
+  if (response === 'y') {
+    console.log('-'.repeat(50));
+    continue;
+  } else {
+    newCalculation = false;
+    console.log('Goodbye!');
+  }
+
+} while (newCalculation);
