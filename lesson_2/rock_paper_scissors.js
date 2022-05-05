ecd45727fb1a0e9bf71e9a3cab9f178e37f268d0
Matt Frazier <frazier3388@yahoo.com>
2022-05-06 07:52:47 +0900

    Completed rock, paper, scissors walk-through

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -5,15 +5,38 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-let choice = rlSync.question();
-
-while (!VALID_CHOICES.includes(choice)) {
-  prompt("That's not a valid choice!");
-  choice = rlSync.question();
+function displayWinner(userChoice, compChoice) {
+  if ((userChoice === 'rock' && compChoice === 'scissors') || (userChoice === 'paper' && compChoice === 'rock') || (userChoice === 'scissors' && compChoice === 'paper')) {
+    prompt('You win!');
+  } else if ((userChoice === 'rock' && compChoice === 'paper') || (userChoice === 'paper' && compChoice === 'scissors') || (userChoice === 'scissors' && compChoice === 'rock')) {
+    prompt('Computer wins!');
+  } else {
+    prompt("It's a tie!");
+  }
 }
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = rlSync.question();
 
-prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice!");
+    choice = rlSync.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  displayWinner(choice, computerChoice);
+
+  prompt('Do you want to play again (y/n)?');
+  let answer = rlSync.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = rlSync.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
+}
