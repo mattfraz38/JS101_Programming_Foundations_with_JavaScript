08c116392f324a80bd7a8bceeb3c640bd29c1cd5
Matt Frazier <frazier3388@yahoo.com>
2022-05-05 20:44:14 +0900

    Initial commit of rock, paper, scissors game

---
+++ lesson_2/rock_paper_scissors.js
@@ -0,0 +1,19 @@
+const rlSync = require('readline-sync');
+const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+let choice = rlSync.question();
+
+while (!VALID_CHOICES.includes(choice)) {
+  prompt("That's not a valid choice!");
+  choice = rlSync.question();
+}
+
+let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+let computerChoice = VALID_CHOICES[randomIndex];
+
+prompt(`You chose ${choice}, computer chose ${computerChoice}`);
