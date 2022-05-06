541e11e3d957566d2d326ba49f5875c525fd3ccc
Matt Frazier <frazier3388@yahoo.com>
2022-05-06 16:00:24 +0900

    Refactor userWin() and compWin() functions and add comments

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,34 +1,41 @@
 const rlSync = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
+// Format responses
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-// rock > scissors, lizard
-// paper > rock, spock
-// scissors > paper, lizard
-// lizard > paper, spock
-// spock > rock, scissors
+// Determine if user wins
+function userWin(user, comp) {
+  return ((user === 'rock' && (comp === 'scissors' || comp === 'lizard')) ||
+    (user === 'paper' && (comp === 'rock' || comp === 'spock')) ||
+    (user === 'scissors' && (comp === 'paper' || comp === 'spock')) ||
+    (user === 'lizard' && (comp === 'paper' || comp === 'spock')) ||
+    (user === 'spock' && (comp === 'rock' || comp === 'scissors')));
+}
 
+// Determine if computer wins
+function compWin(user, comp) {
+  return ((user === 'rock' && (comp === 'paper' || comp === 'spock')) ||
+    (user === 'paper' && (comp === 'scissors' || comp === 'lizard')) ||
+    (user === 'scissors' && (comp === 'rock' || comp === 'spock')) ||
+    (user === 'lizard' && (comp === 'rock' || comp === 'scissors')) ||
+    (user === 'spock' && (comp === 'paper' || comp === 'lizard')));
+}
+
+// Display the winner
 function displayWinner(userChoice, compChoice) {
-  if ((userChoice === 'rock' && (compChoice === 'scissors' || compChoice === 'lizard')) ||
-    (userChoice === 'paper' && (compChoice === 'rock' || compChoice === 'spock')) ||
-    (userChoice === 'scissors' && (compChoice === 'paper' || compChoice === 'spock')) ||
-    (userChoice === 'lizard' && (compChoice === 'paper' || compChoice === 'spock')) ||
-    (userChoice === 'spock' && (compChoice === 'rock' || compChoice === 'scissors'))) {
+  if (userWin(userChoice, compChoice)) {
     prompt('You win!');
-  } else if ((userChoice === 'rock' && (compChoice === 'paper' || compChoice === 'spock')) ||
-    (userChoice === 'paper' && (compChoice === 'scissors' || compChoice === 'lizard')) ||
-    (userChoice === 'scissors' && (compChoice === 'rock' || compChoice === 'spock')) ||
-    (userChoice === 'lizard' && (compChoice === 'rock' || compChoice === 'scissors')) ||
-    (userChoice === 'spock' && (compChoice === 'paper' || compChoice === 'lizard'))) {
+  } else if (compWin(userChoice, compChoice)) {
     prompt('Computer wins!');
   } else {
     prompt("It's a tie!");
   }
 }
 
+// Begin game loop
 while (true) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = rlSync.question();
@@ -38,6 +45,7 @@ while (true) {
     choice = rlSync.question();
   }
 
+  // Calculate computers choice
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
@@ -47,10 +55,14 @@ while (true) {
 
   prompt('Do you want to play again (y/n)?');
   let answer = rlSync.question().toLowerCase();
+
   while (answer[0] !== 'n' && answer[0] !== 'y') {
     prompt('Please enter "y" or "n".');
     answer = rlSync.question().toLowerCase();
   }
 
-  if (answer[0] !== 'y') break;
+  if (answer[0] !== 'y') {
+    prompt('Goodbye!');
+    break;
+  }
 }
