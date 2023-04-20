e80c17a732e6b793451cabfff3b31e4706439248
Matt Frazier <frazier3388@yahoo.com>
2023-04-20 15:06:07 +0900

    add to .gitignore and advanced problems #1

--- remote_test/newFile.js
+++ remote_test/newFile.js
@@ -1,1 +1,4 @@
 console.log('hello from iPad');
+
+// this is a comment done from vim
+// I want to start using vim
--- small_problems/advanced_1/1.js
+++ small_problems/advanced_1/1.js
@@ -1,6 +1,5 @@
 /*
   Madlibs Revisited
-
   QUESTIONS:
     - Am I returning one string on many lines? or many strings?
   INPUT:
@@ -28,6 +27,14 @@ let template1 =
   "%{verb} the %{adjective} yellow " +
   "%{noun}, who %{adverb} %{verb} his " +
   "%{noun} and looks around.";
+
+// this is also ok
+// `
+// The %{adjective} brown %{noun} %{adverb}
+// %{verb} the %{adjective} yellow
+// %{noun}, who %{adverb} %{verb} his
+// %{noun} and looks around.
+// `;
 
 let template2 = "The %{noun} %{verb} the %{noun}'s %{noun}.";
 
@@ -70,3 +77,7 @@ console.log(madlibs(template1));
 console.log(madlibs(template2));      // The "fox" "bites" the "dog"'s "tail".
 
 console.log(madlibs(template2));      // The "cat" "pats" the "cat"'s "head".
+
+/*
+The solution processes the template by calling the String.prototype.replace method and passing in a regex and a callback function as arguments. The regex pattern, /\%{[a-z]+}/g, matches each token in the template. The text matched by the regex pattern is then passed as an argument to the replaceText callback function. The replaceText function processes the matched text to remove any characters used as identifiers (in this case, %{}), leaving only the content type. The content type is then used as the key to retrieve the appropriate list of words from the REPLACEMENT_TEXTS object. The replaceText function generates a random index and uses it to select a word from the list at random.
+*/
